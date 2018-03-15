b5f23227351d3d57bc1f57132346686d088b0845
Sergey <seruy722@gmail.com>
2018-03-15 19:29:47 +0200

    edit create and update function in TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -26,23 +26,12 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
-        $this->validCreate($request);
+        $this->validFields($request);
 
-        $genre = $request->only('genre');
-        $genre = implode(',', $genre['genre']);
         $arr = $request->all();
-        $arr['genre'] = $genre;
-
-        if ($request->hasFile('file')) {
-            $image = $request->file('file');
-            $fileName = $image->getClientOriginalName();
-            $destinationPath = public_path('/images');
-            $image->move($destinationPath, $fileName);
-            $arr['file_name'] = $fileName;
-        } else {
-            $arr['file_name'] = 'nofoto.jpg';
-        }
-
+        $arr['file_name'] = $this->checkUploadFile('file',$request);
+        $arr['genre'] = $this->arrayToString('genre', $request);
+        
         Task::create($arr);
         return redirect()->route('tasks.index');
     }
@@ -55,24 +44,18 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
-        $this->validUpdate($request,$id);
+        $this->validFields($request, $id);
 
-        $genre = $request->only('genre');
-        $genre = implode(',', $genre['genre']);
         $arr = $request->all();
-        $arr['genre'] = $genre;
+        $file = $this->checkUploadFile('file',$request,$id);
 
-        if ($request->hasFile('file')) {
-            $image = $request->file('file');
-            $fileName = $image->getClientOriginalName();
-            $destinationPath = public_path('/images');
-            $image->move($destinationPath, $fileName);
-            $arr['file_name'] = $fileName;
-        } else {
-            $arr = $request->except('file_name');
-            $arr['genre'] = $genre;
+        if(is_string($file)){
+            $arr['file_name'] = $file;
+        }else{
+            $arr = $file;
         }
 
+        $arr['genre'] = $this->arrayToString('genre', $request);
         $myTask = Task::find($id);
         $myTask->fill($arr);
         $myTask->save();
@@ -91,45 +74,44 @@ class TasksController extends Controller
         return redirect()->route('tasks.index');
     }
 
-    public function validCreate($req)
-    {
-        $this->validate($req, [
-            'surname' => 'required|min:2|max:100',
-            'name' => 'required|string|max:100|min:2',
-            'email' => 'required|string|email|max:100|unique:tasks',
-            'phone' => 'required|digits:10|unique:tasks',
-            'day' => 'required',
-            'genre' => 'required',
-            'comment' => 'required',
-            'file' => 'image',
-        ], [
-            'surname.required' => 'Поле Фамилия незаполнено.',
-            'surname.min' => 'Поле Фамилия не может быть меньше 2 символов.',
-            'surname.max' => 'Поле Фамилия не может быть больше 100 символов.',
-            'name.required' => 'Поле Имя незаполнено.',
-            'name.max' => 'Поле Имя не может быть больше 100 символов.',
-            'name.min' => 'Поле Имя не может быть меньше 2 символов.',
-            'email.required' => 'Поле Email незаполнено.',
-            'email.email' => 'Поле Email содержит невалидный адрес.',
-            'email.max' => 'Поле Email не может быть больше 100 символов.',
-            'email.unique' => 'Email адрес занят.',
-            'phone.required' => 'Поле Телефон незаполнено.',
-            'phone.digits' => 'Поле Телефон должно состоять из 10 цыфр.',
-            'phone.unique' => 'Номер телефона занят.',
-            'day.required' => 'Поле День недели незаполнено.',
-            'genre.required' => 'Выберете минимум 1 жанр фильма.',
-            'comment.required' => 'Поле комментарий незаполнено.',
-            'file.image' => 'Файл должен быть изображением',
-        ]);
+    public function checkUploadFile($file,$req,$id=null){
+        if ($req->hasFile($file)) {
+            $image = $req->file($file);
+            $fileName = $image->getClientOriginalName();
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $fileName);
+        }
+            
+        if($id || $req->hasFile($file)){
+            return $fileName;
+        }
+        if($id && $req->hasFile($file)){
+           return $fileName;
+        }
+        if($id && !($req->hasFile($file))){
+            return $arr = $req->except('file_name');
+        }
+
+        
+            return 'nofoto.jpg';
+        
+        
     }
 
-    public function validUpdate($req,$id)
+    public function arrayToString($field, $req)
+    {
+        $arr = $req->only($field);
+        $str = implode(',', $arr[$field]);
+        return $str;
+    }
+
+    public function validFields($req, $id = '')
     {
         $this->validate($req, [
             'surname' => 'required|min:2|max:100',
             'name' => 'required|string|max:100|min:2',
-            'email' => 'required|string|email|max:100|unique:tasks,email,'.$id,
-            'phone' => 'required|digits:10|unique:tasks,phone,'.$id,
+            'email' => 'required|string|email|max:100|unique:tasks,email,' . $id,
+            'phone' => 'required|digits:10|unique:tasks,phone,' . $id,
             'comment' => 'nullable',
             'day' => 'required',
             'genre' => 'required',
